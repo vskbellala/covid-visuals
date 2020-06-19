4728261071c38bc5daec6191a3386608ffd10666
arch7963-alt <66971184+arch7963-alt@users.noreply.github.com>
2020-06-19 14:20:32 -0500

    updates to original heatmap & new heatmap based on counties(don't use)

--- heatmap-testing/heatmap-animation-test.py
+++ heatmap-testing/heatmap-animation-test.py
@@ -3,20 +3,23 @@ import plotly.graph_objects as go
 import pandas as pd
 
 url = 'https://covidtracking.com/api/v1/states/daily.csv'
-data = pd.read_csv(url, usecols=['date', 'state', 'positive'])
+data = pd.read_csv(url, parse_dates=['date'], usecols=['date', 'state', 'positive'])
 df = pd.DataFrame(data)
 df = df[::-1]
+df['date'] = df['date'].dt.strftime('%m/%d')
+df['date'] = df['date'].astype('str')
 
 fig = px.choropleth(df,
-                    animation_frame='date',
                     locationmode='USA-states',
                     locations='state',
+                    animation_frame='date',
                     color='positive',
                     color_continuous_scale='Emrld',
                     range_color=(0, df['positive'].max()),
-                    hover_name='state'
+                    labels={'positive': 'Positive Cases',
+                            'date': 'Date',
+                            'state': 'State'}
                     )
-
 
 fig.update_layout(
     title_text='COVID-19 Heatmap',
@@ -26,5 +29,12 @@ fig.update_layout(
         showlakes=True,
         lakecolor='rgb(255, 255, 255)')
 )
+
+fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 100
+fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 100
+
+# Update layout and stuff with print(fig.layout)
+
+# fig.data[0].update(hovertemplate='State: %{location}<br>Positive Cases: %{z}<br>Date: 01/22<extra></extra>')
 
 fig.show()
