e846194d89196da28f864f6b79379f2672ba2f3b
Venkatsai Bellala <42386481+vskbellala@users.noreply.github.com>
2020-08-21 14:28:45 -0500

    test new timeline script

--- docs/assets/scripts.js
+++ docs/assets/scripts.js
@@ -3,34 +3,53 @@
         headerIds: false
     });
 
+    //Master Path for MD content
+    let mpath = 'https://bellala.org/covid-visuals/content/';
+
     //Function for reading MD file
     function readFile(file, out) {
         var http = new XMLHttpRequest();
-        var tpath = 'https://bellala.org/covid-visuals/content/' + file;
-        http.open('get', tpath);
+        http.open('get', mpath+file);
         http.onreadystatechange = function() {
             document.getElementById(out).innerHTML = marked(http.responseText); //.replace(/\n/g, '<br>'));
         };
         http.send();
     }
+
+    // Functions + Helpers for dynamic Timeline construction
+
     const parseHTMLString = (() => {
         const parser = new DOMParser();
         return str => parser.parseFromString(str, "text/html");
     })();
-    //Function for reading MD file for timeline
-    function readTime(file, out) {
-        var http = new XMLHttpRequest();
-        var tpath = 'https://bellala.org/covid-visuals/content/' + file;
-        http.open('get', tpath);
-        http.onreadystatechange = function() {
-            var mark = marked(http.responseText);
-            document.getElementById(out).innerHTML = mark; //.replace(/\n/g, '<br>'));
-            var mhtml = parseHTMLString(mark);
-            document.getElementById(out).setAttribute("date-is", mhtml.querySelector("h6").innerHTML);
-        };
-        http.send();
+
+    function addTime(entries) {
+        let time = document.getElementById("timeline");
+        if (time != null) {
+            time.innerHTML = "";
+            for (let i = 0; i < entries.length; i++) {
+                let mhtml = parseHTMLString(entries[i].html);
+                time.innerHTML += "<div class='timeline-item' date-is='"+mhtml.querySelector("h6").innerHTML+"''>"+entries[i].html+"</div>";
+            }
+        }
     }
 
+    function makeTimeline(files,path) {
+        let entries = [];
+        for (let f = 0; f < files.length; f++) {
+            let client = new XMLHttpRequest();
+            client.open('GET', mpath+path+files[f]);
+            client.send();
+            client.onreadystatechange = function() {
+                if (client.readyState != 4) return;
+                let html = marked(client.responseText);
+                entries.push({ html: html, url: client.responseURL });
+                addTime(entries);
+            }
+        }
+    }
+
+    // for easier plotly embeds
     $.fn.embed.settings.sources = {
         plotly: {
             name: 'plotly',
@@ -50,3 +69,20 @@
             }
         },
     };
+
+    /*
+    Deprecated timeline script
+    //Function for reading MD file for timeline
+    function readTime(file, out) {
+        var http = new XMLHttpRequest();
+        http.open('get', mpath+file);
+        http.onreadystatechange = function() {
+            var mark = marked(http.responseText);
+            document.getElementById(out).innerHTML = mark; //.replace(/\n/g, '<br>'));
+            var mhtml = parseHTMLString(mark);
+            document.getElementById(out).setAttribute("date-is", mhtml.querySelector("h6").innerHTML);
+        };
+        http.send();
+    }
+
+    */
---
+++ docs/content/deathmap.md
@@ -0,0 +1,2 @@
+###### Visualization: COVID-19 Deaths
+<div class="ui embed" data-source="plotly" data-id="deaths_heat" data-placeholder="../content/images/plotly_logo.webp"></div>
