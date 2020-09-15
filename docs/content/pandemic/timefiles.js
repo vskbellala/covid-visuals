783d1574d5e67890776abde2b047ce1090785770
Venkatsai Bellala <42386481+vskbellala@users.noreply.github.com>
2020-09-15 13:43:29 -0500

    Added many style changes
    cut img width on non-mobile screens by half
    replaced plotly placeholder with edited graphic from pixabay
    added AOS lib to timeline
    moved timefiles array for main timeline to its own js file for ease of use

---
+++ docs/content/pandemic/timefiles.js
@@ -0,0 +1,14 @@
+var timefiles = [
+    'January1', 'January17', 'January21', 'January24',
+    'deaths_heat', 'January27', 'January30', 'January31',
+    'February2', 'February5', 'February7', 'February12',
+    'February15', 'February24', 'bubble_chart', 'February25', 'February29',
+    'March3', 'March4', 'March6', 'March13', 'March17',
+    'March18', 'March19', 'March20', 'March27', 'March28',
+    'March29', 'April3', 'April4', 'cases_pie', 'April5', 'April16',
+    'April17', 'April21', 'April23', 'April24', 'April26',
+    'April27', 'April29', 'April30', 'bubble_map', 'May1', 'May2',
+    'May6', 'May7', 'covid_lines', 'May8', 'May11', 'May14', 'May15', 'May21', 'May25', 'May26',
+    'June2', 'June4', 'June9', 'June10', 'June11', 'June15', 'June20', 'June22',
+    'June23', 'June27', 'July3', 'July5', 'deaths_heat', 'July12', 'July13', 'July14', 'July15', 'July22', 'region_bar'
+];
--- docs/content/pandemic/timeline/bubble_chart.md
+++ docs/content/pandemic/timeline/bubble_chart.md
@@ -1,3 +1,3 @@
 <div class="ui embed" data-source="plotly" data-id="bubble_chart" data-placeholder="../content/images/plotly_logo.webp"><i class="play icon"></i><img class="placeholder" src="../content/images/plotly_logo.webp"></div>
 
-###### Visualization: COVID-19 Tests vs. Positive Cases vs. Deaths in the US by State
+###### Visualization: COVID-19 Bubble Chart
