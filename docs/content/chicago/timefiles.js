9515ae556574085f94b6d4957b4d61de0aa7346e
Venkatsai Bellala <42386481+vskbellala@users.noreply.github.com>
2020-09-21 16:49:41 -0500

    Add chicago & nyc case studies
    Co-Authored-By: samanyuk25 <26726376+samanyuk25@users.noreply.github.com>
    Co-Authored-By: koushik198 <66970051+koushik198@users.noreply.github.com>

---
+++ docs/content/chicago/timefiles.js
@@ -0,0 +1,13 @@
+var timefiles = [
+    'January31',
+    'March11',
+    'March13',
+    'March15',
+    'March17',
+    'March20',
+    'April25',
+    'May1',
+    'May7',
+    'May30',
+    'June3'
+];
---
+++ docs/content/nyc/timefiles.js
@@ -0,0 +1,18 @@
+var timefiles = [
+    'March7',
+    'March8',
+    'March12',
+    'March16',
+    'March17',
+    'March20',
+    'March30',
+    'April1',
+    'April14',
+    'April22',
+    'June1',
+    'June8',
+    'June24',
+    'July26',
+    'July31',
+    'August5'
+];
