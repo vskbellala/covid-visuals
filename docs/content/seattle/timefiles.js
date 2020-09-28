d64495b004a7d22439f719a2b541b5b1dc6ddf43
Venkatsai Bellala <42386481+vskbellala@users.noreply.github.com>
2020-09-28 10:04:12 -0500

    Add Seattle case study
    Create timefiles.js for Seattle
    
    Co-Authored-By: koushik198 <66970051+koushik198@users.noreply.github.com>

---
+++ docs/content/seattle/timefiles.js
@@ -0,0 +1,14 @@
+var timefiles = [
+    'February29',
+    'March3',
+    'March4',
+    'March13',
+    'March16',
+    'March23',
+    'April5',
+    'April19',
+    'May1',
+    'May29',
+    'June1',
+    'June19'
+];
