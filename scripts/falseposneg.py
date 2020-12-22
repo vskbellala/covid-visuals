c2b7a65692bcd377eedf824b022f7150b39893b2
Venkatsai Bellala <42386481+vskbellala@users.noreply.github.com>
2020-12-22 13:39:27 -0600

    falseposneg draft
    Co-Authored-By: samanyuk25 <26726376+samanyuk25@users.noreply.github.com>

--- scripts/falseposneg.py
+++ scripts/falseposneg.py
@@ -6,5 +6,11 @@ import pandas as pd
 
 url = 'https://api.covidtracking.com/v1/states/current.csv' # current COVID 19 data for US states
 
-state_cases = pd.read_csv(url,parse_dates=['date'], usecols=['date','state','totalTestsPeopleViral','negative','positiveCasesViral','positive'])
+state_cases = pd.read_csv(url,parse_dates=['date'],
+    usecols=['date','totalTestsViral','negativeTestsViral','positiveTestsViral','positive','negativeIncrease'])
 
+df = pd.DataFrame(state_cases)
+df = df.fillna(0)
+
+df_agg = df.agg('sum')
+print(df_agg)
