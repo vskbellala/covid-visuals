2f32895b34d1ccf965bdea585dd83922a4fc2d09
arch7963-alt <66971184+arch7963-alt@users.noreply.github.com>
2020-12-28 13:23:34 -0600

    updated heatmap_percenthospitalized.py

--- scripts/heatmap_percenthospitalized.py
+++ scripts/heatmap_percenthospitalized.py
@@ -5,14 +5,14 @@ import pandas as pd
 import us
 
 url = 'https://api.covidtracking.com/v1/states/daily.csv'
-data = pd.read_csv(url, parse_dates=['date'], usecols=['date', 'hospitalizedIncrease', 'state', 'positiveIncrease'])
+data = pd.read_csv(url, parse_dates=['date'], usecols=['date', 'hospitalizedCumulative', 'state', 'positive'])
 df = pd.DataFrame(data)
 df = df[::-1]
 
 df['date'] = df['date'].dt.strftime('%B %d, %Y')
 df['date'] = df['date'].astype('str')
 
-df['percent'] = df['hospitalizedIncrease']/df['positiveIncrease']*100
+df['percent'] = (df['hospitalizedCumulative']/df['positive'])*100
 
 df = df.fillna(0)
 
@@ -20,9 +20,9 @@ fig = px.choropleth(df,
                     locationmode='USA-states',
                     locations='state',
                     animation_frame='date',
-                    color=df['percent'],
+                    color='percent',
                     color_continuous_scale='deep',
-                    range_color=(0, df['percent'].max()),
+                    range_color=(0, 40),
                     labels={'percent': 'Percent of Positive Cases Hospitalized',
                             'date': 'Date',
                             'state': 'State'},
@@ -55,20 +55,14 @@ fig.update_layout(
             thicknessmode='pixels',
             thickness=12,
             title=dict(
-                text='Percent*'
-                ),
-            tickvals=[0, 1, 2, 3],
-            ticktext=[0, 10, 20, 30]
+                text='Percent'
+                )
+            #tickvals=[0, 1, 2, 3],
+            #ticktext=[0, 10, 20, 30]
             )
         ),
     font_family='Rockwell', # Font for plot
-    paper_bgcolor='#ffffff', # Background color of whole thing
-    annotations=[dict(
-        text='*Positive Cases Hospitalized Per State',
-        showarrow=False,
-        x=0.02,
-        y=-0.07
-        )]
+    paper_bgcolor='#ffffff' # Background color of whole thing
 )
 # Updates the layout of the heatmap
 
@@ -83,17 +77,17 @@ a_opts = {"frame": {"duration": 110}, "transition": {"duration": 110}}
 fig.layout.sliders[0].currentvalue['prefix'] = 'Date: '
 # Reformats the "Date" on the slider
 
-fig.update_traces(hovertemplate='<b>%{hovertext}</b><br>Date: %{customdata[0]}<br>Percent*: %{customdata[1]}<extra></extra>')
+fig.update_traces(hovertemplate='<b>%{hovertext}</b><br>Date: %{customdata[0]}<br>Percent: %{customdata[1]}<extra></extra>')
 for frame in fig.frames:
-    frame['data'][0].hovertemplate = '<b>%{hovertext}</b><br>Date: %{customdata[0]}<br>Percent*: %{customdata[1]}<extra></extra>'
+    frame['data'][0].hovertemplate = '<b>%{hovertext}</b><br>Date: %{customdata[0]}<br>Percent: %{customdata[1]}<extra></extra>'
 
 fig.layout.updatemenus[0].showactive = True
 fig.layout.sliders[0].tickcolor = '#ffffff'  # Blends ticks in with background
 fig.layout.coloraxis.colorbar.title.font.size = 15
 
-fig.show() 
+# fig.show() 
 
-# fig.write_html(file="../docs/plots/percent_hospitalized.html",auto_play=True,full_html=False,include_plotlyjs='cdn',
-#   animation_opts=a_opts
+fig.write_html(file="../docs/plots/percent_hospitalized.html",auto_play=True,full_html=False,include_plotlyjs='cdn',
+   animation_opts=a_opts
 
-# ) # write figure to html
+) # write figure to html
